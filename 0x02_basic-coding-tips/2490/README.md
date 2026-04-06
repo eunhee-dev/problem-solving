dd0a0194741f815025118b2ff82708e5279a0a4b
Eunhee Kim <eunee.stella@gmail.com>
2026-04-06 13:29:55 +0900

    feat: add solution for 0x02-2490

--- 0x02_basic-coding-tips/2490/README.md
+++ 0x02_basic-coding-tips/2490/README.md
@@ -11,7 +11,7 @@ https://www.acmicpc.net/problem/2490
 
 
 2. 등의 개수에 따른 결과 매핑: `{0: 'D', 1: 'C', 2: 'B', 3: 'A', 4: 'E'}` 또는 인덱싱 `"DCBAE"[합]`으로 처리 가능하다.
-
+    - 딕셔너리 혹은 리스트의 인덱스를 이용해 접근하면 코드의 간결함을 유지할 수 있음
 
 3. list comprehension을 통해 간결하게 return이 가능하다.
 
@@ -27,3 +27,5 @@ https://www.acmicpc.net/problem/2490
     def solve(yuts: list[list[int]]) -> list[str]:
         return ["DCBAE"[sum(yut)] for yut in yuts]
     ```
+
+4. `solve` 함수 자체에서 정답을 리턴하게 하고, `main` 함수에서 `print`를 이용해서 출력
--- 0x02_basic-coding-tips/2490/solve.py
+++ 0x02_basic-coding-tips/2490/solve.py
@@ -1,22 +1,15 @@
-""" solve.py for 2490번. 윷놀이 """
-
 import sys
 
-
-def sys_input() -> str:
+def sys_input():
     return sys.stdin.readline().rstrip()
 
-
-def solve(yuts: list[list[int]]) -> list[str]:
+def solve(yuts):
     return ["DCBAE"[sum(yut)] for yut in yuts]
 
-
-def main() -> None:
+def main():
     yuts = [list(map(int, sys_input().split())) for _ in range(3)]
-
-    answer: list[str] = solve(yuts)
+    answer = solve(yuts)
     print(*answer, sep="\n")
-
 
 if __name__ == "__main__":
     main()
