58111e383cfac2cd2773f6e4c3a9626fc18cb274
Eunhee Kim <eunee.stella@gmail.com>
2026-04-06 10:40:46 +0900

    feat: add solution for 0x02-2752

--- 0x02_basic-coding-tips/2752/solve_sort.py
+++ 0x02_basic-coding-tips/2752/solve_sort.py
@@ -1,22 +1,15 @@
-""" solve_sort.py for 2752번. 세수정렬 """
-
 import sys
 
-
-def sys_input() -> str:
+def sys_input():
     return sys.stdin.readline().rstrip()
 
-
-def solve(a: int, b: int, c: int) -> list[int]:
-    return sorted([a, b, c])
-
-
-def main() -> None:
-    a, b, c = map(int, sys_input().split())
-
-    answer: list[int] = solve(a, b, c)
-    print(*answer)
-
+def solve():
+    numbers = [item for item in map(int, sys_input().split())]
+    numbers.sort()
+    print(*numbers)
+    
+def main():
+    solve()
 
 if __name__ == "__main__":
     main()
