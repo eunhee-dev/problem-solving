c3ad438bee0cf9cd82e9162151d08e12e9f83eca
Eunhee Kim <eunee.stella@gmail.com>
2026-04-08 09:19:23 +0900

    feat: add solution for 0x02-2587

--- 0x02_basic-coding-tips/2587/solve.py
+++ 0x02_basic-coding-tips/2587/solve.py
@@ -1,23 +1,20 @@
-""" solve.py for 2587번. 대표값2 """
-
 import sys
 
-
-def sys_input() -> str:
+def sys_input():
     return sys.stdin.readline().rstrip()
 
+def solve():
+    numbers = [int(sys_input()) for _ in range(5)]
+    numbers.sort()
+    
+    mean = sum(numbers)//5
+    median = numbers[2]
 
-def solve(nums: list[int]) -> tuple[int, int]:
-    avg = sum(nums) // len(nums)
-    median = sorted(nums)[len(nums) // 2]
-    return avg, median
+    return [mean, median]
 
 
-def main() -> None:
-    nums = [int(sys_input()) for _ in range(5)]
-
-    answer: tuple[int, int] = solve(nums)
-    print(*answer, sep="\n")
+def main():
+    print(*solve(), sep="\n")
 
 
 if __name__ == "__main__":
